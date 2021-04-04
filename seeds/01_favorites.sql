f9c111d72a30c33769125a8d9cdafe58f424ebec
Josiah <josiah.pilon@yahoo.ca>
2021-04-04 04:48:50 +0000

    Update user id

--- seeds/01_favorites.sql
+++ seeds/01_favorites.sql
@@ -1,10 +1,10 @@
 INSERT INTO favorites (song, artist, album, genre, user_id)
 VALUES
-  ('Dark Road', 'John Jay Jones', 'Revelations', 'Rock', 6),
-  ('Midnight Truth', 'The Legion', 'Sweet Love','Jazz', 7),
-  ('Homiez', 'Da streetz', 'Atlanta', 'Rap/HipHop', 8),
-  ('Roit', 'Def Lips', 'Unshackle', 'Punk Rock', 9),
-  ('Crossroads', 'Baby Blues', 'Broken Heart', 'Blues', 10);
+  ('Dark Road', 'John Jay Jones', 'Revelations', 'Rock', 1),
+  ('Midnight Truth', 'The Legion', 'Sweet Love','Jazz', 2),
+  ('Homiez', 'Da streetz', 'Atlanta', 'Rap/HipHop', 3),
+  ('Roit', 'Def Lips', 'Unshackle', 'Punk Rock', 4),
+  ('Crossroads', 'Baby Blues', 'Broken Heart', 'Blues', 5);
 
 
 
