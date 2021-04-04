380d9ecaed52c144556bab7833a9683f897f1f55
Luke Bergmann <lukebergmann0@gmail.com>
2021-04-04 03:51:07 +0000

    add songs seeds

---
+++ seeds/03_songs.sql
@@ -0,0 +1,8 @@
+INSERT INTO songs (song_name, price, duration, user_id, artist_id)
+
+VALUES
+('Kanye Rest',500,7.25,6,1),
+('Oud of the blue',500,6.42,7,2),
+('Happy and True',500,5.29,8,3),
+('Battery',500,5.13,9,3),
+('Dialectic Chaos',500,2.26,10,3);
