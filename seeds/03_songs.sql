d002dc9bb57d8f1867bafa92d2b2adbcfb10734d
faroukalsajee <faroukalsajee@gmail.com>
2021-04-04 04:20:05 +0000

    test data

--- seeds/03_songs.sql
+++ seeds/03_songs.sql
@@ -1,8 +1,8 @@
 INSERT INTO songs (song_name, price, duration, user_id, artist_id)
 
 VALUES
-('Kanye Rest',500,7.25,6,1),
-('Oud of the blue',500,6.42,7,2),
-('Happy and True',500,5.29,8,3),
-('Battery',500,5.13,9,3),
-('Dialectic Chaos',500,2.26,10,3);
+('Kanye Rest',500,7.25,1,1),
+('Oud of the blue',500,6.42,2,2),
+('Happy and True',500,5.29,3,3),
+('Battery',500,5.13,4,3),
+('Dialectic Chaos',500,2.26,5,3);
