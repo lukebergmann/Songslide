f56e7b14eb87b2248c4e4afdb82e5e3516e628ff
Josiah <josiah.pilon@yahoo.ca>
2021-04-04 19:51:52 +0000

    Update seeds

--- seeds/01_carts.sql
+++ seeds/01_carts.sql
@@ -2,12 +2,6 @@ INSERT INTO carts (donate, user_id)
 VALUES
 (100, 1),
 (500, 2),
-<<<<<<< HEAD
 (275, 2),
 (150, 3),
 (150, 4);
-=======
-(275, 3),
-(150, 4),
-(150, 5);
->>>>>>> d002dc9bb57d8f1867bafa92d2b2adbcfb10734d
