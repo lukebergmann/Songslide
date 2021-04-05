63fbd8585ce5aa04a0e4f6da9b8659a3b99ff5f5
Josiah <josiah.pilon@yahoo.ca>
2021-04-05 22:10:17 +0000

    returns the resulting rows from the query promises

--- server/database.js
+++ server/database.js
@@ -1,31 +1,49 @@
+// example get request
+// app.get("library/:username/:artist", (req, res) => {})
+// app.get("library/:username/:genre", (req, res) => {})
+
+
+
 // HOMEPAGE
 // select all songs of a genre rock
 // select all songs of a genre jazz
 // select all songs of a genre rap/hiphop
 // select all songs of a genre punkrock
 
+
 // select all songs by genre
-const getSongsByGenre = function(values) {
-  const values = ;
-  Pool.query(`
+// ** question about token
+const getSongsByGenre = function(genre) {
+  return Pool.query(`
   SELECT songs.title AS song_title, songs.artist AS artist_name, songs.duration AS duration, songs.price AS price
   FROM songs
   WHERE genre LIKE %$1%
   // do you need to put percents around the token??
   ORDER BY price
-  LIMIT 5;`)
+  LIMIT 5;`, genre)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
 // select bios by artist
-const getBioByArtist = function(values) {
-  const values = ;
-  Pool.query(`
+const getBioByArtist = function(artist) {
+  return Pool.query(`
   SELECT songs.title AS song_title, artists.name AS artist_name, songs.album AS album, artists.bio AS bio
   FROM songs
   JOIN artists ON artists.id = artist_id
   WHERE artist LIKE %$1%
   GROUP BY artists.name
-  LIMIT 1;`, values)
+  LIMIT 1;`, artist)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
 // select songs by album
@@ -33,26 +51,36 @@ const getBioByArtist = function(values) {
 
 
 // select all songs of an artist
-const getSongsByArtist = function(values) {
-  const values = ;
-  Pool.query(`
+const getSongsByArtist = function(name) {
+  return Pool.query(`
   SELECT songs.title AS song_title, songs.artist AS artist_name, songs.duration AS duration, songs.price AS price
   FROM songs
   WHERE artists.name = $1
   GROUP BY artists.name
   ORDER BY price
-  LIMIT 5;`)
+  LIMIT 5;`, name)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
 // select song by price
-const getSongsByPrice = function(values) {
-  const values = ;
-  Pool.query(`
+const getSongsByPrice = function(price) {
+  return Pool.query(`
   SELECT songs.title AS song_title, songs.artist AS artist_name, songs.duration AS duration, songs.price AS price
   FROM songs
   WHERE songs.price <= $1
   ORDER BY price
-  LIMIT 5;`)
+  LIMIT 5;`, price)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
 // USER PAGE
@@ -65,80 +93,114 @@ const getSongsByPrice = function(values) {
 
 // **I think users to artists is a many to many relationship **
 // all users songs
-const getUsersSongs = function(values) {
-  const values = ;
-  Pool.query(`
+const getUsersSongs = function(id) {
+  return Pool.query(`
   SELECT songs.title AS song_title, songs.artist AS artist_name, songs.duration AS duration
   FROM users
   JOIN songs ON users.id = user_id
   WHERE users.id = $1
   ORDER BY artist_name
-  LIMIT 5;`)
+  LIMIT 5;`, id)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
 // user searches for songs by artist
-const getUsersSongsByArtist = function(values) {
-  const values = ;
-  Pool.query(`
+const getUsersSongsByArtist = function(id, name) {
+  const values = [id, name];
+  return Pool.query(`
   SELECT songs.title AS song_title, songs.artist AS artist_name, songs.duration AS duration
   FROM users
   JOIN songs ON users.id = user_id
   WHERE users.id = $1 AND songs.artist = $2
   ORDER BY artist_name
-  LIMIT 5;`)
+  LIMIT 5;`, values)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
 // ** change favorites.song to favorites.title **
 // users favorite songs
-const getUsersFavoriteSongs = function(values) {
-  const values = ;
-  Pool.query(`
+const getUsersFavoriteSongs = function(id) {
+  return Pool.query(`
   SELECT favorites.title AS title, favorites.artist AS artist, favorites.album AS album, favorites.genre AS genre
   FROM users
   JOIN favorites ON users.id = user_id
   GROUP BY users.id
   WHERE users.id = $1
   ORDER BY title
-  LIMIT 5;`)
+  LIMIT 5;`, id)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
 // users favorite songs by artist
-const getUsersFavoriteSongsByArtits = function(values) {
-  const values = ;
-  Pool.query(`
+const getUsersFavoriteSongsByArtits = function(id, name) {
+  const values = [id, name];
+  return Pool.query(`
   SELECT favorites.title AS title, favorites.artist AS artist, favorites.album AS album, favorites.genre AS genre
   FROM users
   JOIN favorites ON users.id = user_id
   WHERE users.id = $1 AND favorites.artist = $2
   GROUP BY users.id AND favorites.artist
   ORDER BY title
-  LIMIT 5;`)
+  LIMIT 5;`, values)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
 // users favorite songs by album
-const getUsersFavoriteSongsByAlbum = function(values) {
-  const values = ;
-  Pool.query(`
+const getUsersFavoriteSongsByAlbum = function(id, album) {
+  const values = [id, album];
+  return Pool.query(`
   SELECT favorites.title AS title, favorites.artist AS artist, favorites.album AS album, favorites.genre AS genre
   FROM users
   JOIN favorites ON users.id = user_id
   WHERE users.id = $1 AND favorites.album = $2
   GROUP BY users.id AND favorites.album
   ORDER BY title
-  LIMIT 5;`)
+  LIMIT 5;`, values)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
 // users favorite songs by genre
-const getUsersFavoriteSongsByGenre = function(values) {
-  const values = ;
-  Pool.query(`
+const getUsersFavoriteSongsByGenre = function(id, genre) {
+  const values = [id, genre];
+  return Pool.query(`
   SELECT favorites.title AS title, favorites.artist AS artist, favorites.album AS album, favorites.genre AS genre
   FROM users
   JOIN favorites ON users.id = user_id
   WHERE users.id = $1 AND favorites.genre = $2
   GROUP BY users.id AND favorites.genre
   ORDER BY title
-  LIMIT 5;`)
+  LIMIT 5;`, values)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
 // ARTISTS PAGE
@@ -147,13 +209,32 @@ const getUsersFavoriteSongsByGenre = function(values) {
 
 // **artists table needs an album**
 const getArtistsSongsByAlbum = function(values) {
-  const values = ;
-  Pool.query(`
+  return Pool.query(`
   SELECT artists.title AS title, artists.price AS price
   FROM artists
   JOIN songs ON artists.id = artist_id
   GROUP BY artists.album
-  ORDER BY artists.title;`)
+  ORDER BY artists.title;`, values)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return undefined;
+    })
 };
 
-module.exports = { getSongsByGenre, getBioByArtist, getSongsByArtist, getSongsByArtist, getSongsByPrice, getUsersSongs, getUsersSongsByArtist, getUsersFavoriteSongs, getUsersFavoriteSongsByArtits, getUsersFavoriteSongsByAlbum, getUsersFavoriteSongsByAlbum, getUsersFavoriteSongsByGenre, getArtistsSongsByAlbum };
+module.exports = {
+  getSongsByGenre,
+  getBioByArtist,
+  getSongsByArtist,
+  getSongsByArtist,
+  getSongsByPrice,
+  getUsersSongs,
+  getUsersSongsByArtist,
+  getUsersFavoriteSongs,
+  getUsersFavoriteSongsByArtits,
+  getUsersFavoriteSongsByAlbum,
+  getUsersFavoriteSongsByAlbum,
+  getUsersFavoriteSongsByGenre,
+  getArtistsSongsByAlbum
+};
