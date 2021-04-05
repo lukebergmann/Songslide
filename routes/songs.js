9c66fe97da9aa9a7659566ad3c49b4ca817b180b
faroukalsajee <faroukalsajee@gmail.com>
2021-04-05 20:38:41 +0000

    create routes and connect to server

---
+++ routes/songs.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+// get/ provides the list of all songs available
+// router.get("/", (req, res) => {
+//   res.render("homepage");
+// });
+
+
+
+
+//get/new to desplay the songs for sale
+//post/ to post to songs
+
+
+
+
+// router.get("/:id", (req, res) => {
+//   let selectedSongs = req.params.id;
+//   console.log('>>>>', selectedSongs);
+// });
+// console.log('>>>>>>>>>');
+// const songId = selectedSongs.song_id;
+// songs[songId] = {};
+// songs[songId].song_name = selectedSongs.song_name;
+// songs[songId].price = selectedSongs.price;
+// songs[songId].duration = selectedSongs.duration;
+// songs[songId].user_id = selectedSongs.user_id;
+// songs[songId].artist_id = selectedSongs.artist_id;
+
+// res.end();
+
+  // non-AJAX route; returns full page of HTML
+  // router.get("/:id", (req, res) => {
+  //   res.render("checkout");
+  // });
+
+  // // JSON-only route for AJAX GET
+  // router.get("/:id", (req, res) => {
+  //   res.json({ selectedSongs });
+  // });
+
+
+
+ // Updates a song
+  // router.put("/:id", (req, res) => {
+  //   db.query(`
+  //     UPDATE songs
+  //     SET song_name = $1,
+  //     price = $2,
+  //     duration = $3,
+  //     user_id = $4,
+  //     artist_id = $5,
+  //     WHERE user_id = $6 RETURNING *;
+  //    `, [ request.params.id,
+  //         request.body.title,
+  //         request.body.song_name,
+  //         request.body.price,
+  //         request.body.duration,
+  //         request.body.user_id,
+  //         request.body.artist_id ])
+  //   .then(({ rows: songs }) => { response.json(songs) })
+  //   .catch(e => console.error(e.stack));
+  //   console.log('>>33>>', songs);
+
+  // });
+
+  // // Creates a new song
+  // router.post("/songs/new", (request, response) => {
+  //   db.query(`
+  //     INSERT INTO songs (song_name, price, duration, user_id, artist_id )
+  //       VALUES ( $1, $2, $3, $4, $5 )
+  //     RETURNING *;
+  //     `, [request.body.song_name,
+  //         request.body.price,
+  //         request.body.duration,
+  //         request.body.user_id,
+  //         request.body.artist_id,])
+  //   .then(({ rows: songs }) => { response.status(201).json(songs);
+  //   })
+  //   .catch(e => console.error(e.stack));
+  // });
+
+  // deletes a song
+  // router.post("/:id/delete", (request, response) => {
+  //   db.query(`
+  //     DELETE FROM songs WHERE id = $1;
+  //     `, [ request.params.id ])
+  //     .then((result) => {
+  //       if (result.rowCount !== 0) {
+  //         response.status(200).json({message: "song deleted successfully"});
+  //       } else {
+  //         response.status(204).json({message: "song not found."});
+  //       }
+  //   })
+  //   .catch(e => console.error(e.stack));
+  // });
+
+
+
+
+
+
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -35,11 +35,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const songRoutes = require("./routes/songs");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use('/songs', songRoutes);
 // Note: mount other resources here, using the same pattern above
 
 
@@ -47,9 +49,18 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  const templateVars = {};
+  db.query('SELECT * FROM songs')
+  .then((result)=> {
+    console.log(result.rows);
+    templateVars.songs = result.rows;
+    res.render("homepage", templateVars);
+
+  })
+  .catch((error) => {console.log(error.message)});
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
+
 });
