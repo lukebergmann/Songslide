282d0e61c39e266c12fa9e7882c2084ff4d875a3
faroukalsajee <faroukalsajee@gmail.com>
2021-04-07 20:48:36 +0000

    added in favorites.ejs

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,21 @@
 
 // Users page requests:
 // POST request that allows the user to unfavorite a song
-// POST request that allows the users to remove a song from their cart
-// POST request that allows the user to checkout (redirect to homepage)
+// POST request that allows the users to remove a song from their cart >> done
+// POST request that allows the user to checkout (redirect to homepage) >> done
+
+
+// users can see featured items on a main feed >> done
+// users can filter items by price,
+// users can favourite items to check up on them later >> done
+// users can send messages to the user that is listing the item
+
+// Admins can:
+
+// post items, which can be seen by others
+// remove items from the site
+// mark items as SOLD!,
+// send a message via app, email, or text back on negotiations in buying the said item
 
 
 const express = require('express');
@@ -15,18 +28,29 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     // console.log('>>>>>>>14');
     const templateVars = {};
-    db.query('SELECT * FROM users')
+    db.query('SELECT * FROM songs')
       .then((result) => {
         console.log(result.rows);
-        templateVars.users = result.rows;
-        res.render("users", templateVars);
+        templateVars.songs = result.rows;
+        res.render("songs", templateVars);
 
       })
-      .catch((error) => { console.log(error.message) });
+      .catch((error) => {
+        console.log(error.message);
+      });
+  });
+
+  // POST request to save song
+  router.post('/', (req, res) => {
+    let users = req.params.users;
+    saveSongs(users)
+      .then((users) => {
+        res.render("users", users);
+      });
   });
 
 
-  // POST request to get favorite songs saved in the database
+  // GET request to get favorite songs saved in the database
   router.get('/user/:user', (req, res) => {
     const users = req.params.users;
     const templateVars = {};
@@ -41,8 +65,56 @@ module.exports = (db) => {
         templateVars.artists = result.rows;
         res.render("users", templateVars);
       })
-      .catch((error) => { console.log(error.message) });
+      .catch((error) => {
+        console.log(error.message);
+      });
   });
+
+  // POST request that allows the users to remove a song from their cart
+  router.post('/', (req, res) => {
+    let deleted = req.body;
+    console.log('inside server. deleting this song: ', deleted);
+    deleteFavorite(deleted)
+      .then((response) => {
+        res.status(201).send(response);
+      })
+      .catch((err) => {
+        res.status(404).send(err);
+      });
+  });
+
+  // users can favourite items to check up on them later
+  router.get('/favorites/:id', (req, res) => {
+    console.log('return 444');
+    const user = req.params.id;
+    const templateVars = {};
+    db.query(` SELECT * FROM favorites WHERE user_id = $1`, [user])
+      .then((result) => {
+        console.log(result.rows);
+        templateVars.favorites = result.rows;
+        res.render("favorites", templateVars);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  });
+
+  // mark your song as sold:
+  router.post("/", (req, res) => {
+    const songId = req.params.listing_id;
+    // console.log("singgg!");
+    if (!songId) {
+      return res.redirect("/");
+    }
+
+    db.markSongAsSold(songId)
+      .then(() => {
+        res.redirect("songs");
+      });
+
+  });
+
+
   return router;
 
 
