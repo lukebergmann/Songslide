b1e846b921cc420c30676bdcd0002f0f40344ef4
Luke Bergmann <lukebergmann0@gmail.com>
2021-04-08 05:31:40 +0000

    worked on uploading song from artist page

--- routes/artists.js
+++ routes/artists.js
@@ -24,37 +24,39 @@ module.exports = db => {
 
 
   // POST request that submits the new song upload info to the database (redirect to homepage)
-  router.post("/", (request, response) => {
-    console.log('Creaaate');
-
+  router.post("/", (req, res) => {
+    const templateVars = {}
+    console.log("req.body ========", req.body.song_url)
     db.query(`
-    INSERT INTO songs (thumbnail_photo_url, song_url, song_name, )
-      VALUES ( $1, $2, $3, $4, $5 )
+    INSERT INTO songs (song_name, song_url, genre)
+      VALUES ( $1, $2, $3)
     RETURNING *;
-    `), ([request.body.song_name,
+    `, [req.body.song_name, req.body.song_url, req.body.genre
     ])
-      .then(({ rows: songs }) => {
-        response.status(201).json(songs);
+    .then((result) => {
+      console.log(result.rows);
+      templateVars.songs = result.rows;
+      res.render("homepage", templateVars);
       })
       .catch(e => console.error(e.stack));
   });
 
 
   // POST request that allows the artist to delete a song from their page
-  router.post("/", (request, response) => {
-    console.log('deleeeeeete');
-    db.query(`
-    DELETE FROM songs WHERE id = $1;
-    `, [request.params.id])
-    .then((result) => {
-      if (result.rowCount !== 0) {
-        response.status(200)({ message: "song deleted successfully" });
-      } else {
-        response.status(204)({ message: "song not found." });
-      }
-    })
-    .catch(e => console.error(e.stack));
-  });
+  // router.post("/", (request, response) => {
+  //   console.log('deleeeeeete');
+  //   db.query(`
+  //   DELETE FROM songs WHERE id = $1;
+  //   `, [request.params.id])
+  //   .then((result) => {
+  //     if (result.rowCount !== 0) {
+  //       response.status(200)({ message: "song deleted successfully" });
+  //     } else {
+  //       response.status(204)({ message: "song not found." });
+  //     }
+  //   })
+  //   .catch(e => console.error(e.stack));
+  // });
   return router;
 
 };
